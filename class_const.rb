43506293e3f2d5fbd970bdfb23e2ef668463ce56
siryu-saito <saito.s.ryu@gmail.com>
2021-04-21 15:21:50 +0000

    [add]class

---
+++ class_const.rb
@@ -0,0 +1,31 @@
+class Car
+  REGION = 'USA'
+  @@count = 0
+  def initialize(name)
+    @name = name
+    @@count += 1
+  end
+
+  def hello
+    puts "Hello! I am #{@name}. #{@@count} instance(s)"
+  end
+
+  def self.info
+    puts "#{@@count} instance(s). Region: #{REGION}"
+  end
+
+end
+
+kitt = Car.new('Kitt')
+# kitt.hello
+Car.info
+
+karr = Car.new('Karr')
+# karr.hello
+Car.info
+
+nakamura = Car.new('Nakamura')
+# nakamura.hello
+Car.info
+
+puts Car::REGION
---
+++ class_val.rb
@@ -0,0 +1,28 @@
+class Car
+  @@count = 0
+  def initialize(name)
+    @name = name
+    @@count += 1
+  end
+  
+  def hello
+    puts "Hello! I am #{@name}. #{@@count} instance(s)"
+  end
+  
+  def self.info
+    puts "#{@@count} instance(s)."
+  end
+  
+end
+
+kitt = Car.new('Kitt')
+# kitt.hello
+Car.info
+
+karr = Car.new('Karr')
+# karr.hello
+Car.info
+
+nakamura = Car.new('Nakamura')
+# nakamura.hello
+Car.info
