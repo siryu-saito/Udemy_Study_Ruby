075e41d41d361d0f9eba58f6caf155404bdb2d64
siryu-saito <saito.s.ryu@gmail.com>
2021-04-21 14:12:59 +0000

    [add]loop

---
+++ loop.rb
@@ -0,0 +1,25 @@
+# i = 0
+
+# loop do
+#   puts i
+#   i += 1
+# end
+
+# i = 0
+
+# while true
+#   puts i
+#   i += 1
+# end
+
+# loop do
+#   puts i
+#   i += 1
+#   break if i == 10
+# end
+
+numbers = [1, 2, 3, 4, 5]
+numbers.each do |n|
+  next if n.odd?
+  puts n
+end
