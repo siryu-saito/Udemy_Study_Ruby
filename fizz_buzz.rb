49071e75bc8d5e8aaccb654334bf8045fd552e65
siryu-saito <saito.s.ryu@gmail.com>
2021-04-21 04:59:41 +0000

    [add]Fizz_Buzz

---
+++ fizz_buzz.rb
@@ -0,0 +1,27 @@
+def Fizz_Buzz(x)
+  if x % 15 == 0
+    'FizzBuzz'
+  elsif x % 3 == 0
+    'Fizz'
+  elsif x % 5 == 0
+    'Buzz'
+  else
+    x.to_s
+  end
+end
+
+puts Fizz_Buzz(1)
+puts Fizz_Buzz(2)
+puts Fizz_Buzz(3)
+puts Fizz_Buzz(4)
+puts Fizz_Buzz(5)
+puts Fizz_Buzz(6)
+puts Fizz_Buzz(7)
+puts Fizz_Buzz(8)
+puts Fizz_Buzz(9)
+puts Fizz_Buzz(10)
+puts Fizz_Buzz(11)
+puts Fizz_Buzz(12)
+puts Fizz_Buzz(13)
+puts Fizz_Buzz(14)
+puts Fizz_Buzz(15)
